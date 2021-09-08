a665337de5400f2fcd7799f7a58f90c9ca03d03a
Johnny <johnny00joe@gmail.com>
2021-09-08 15:40:14 +0000

    First database migrations

---
+++ database/migrations/2021_09_08_142740_create_sales_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('seller_id')->unsigned();
+            $table->decimal('price', $precision = 10, $scale = 2);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('seller_id')->references('id')->on('sellers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
